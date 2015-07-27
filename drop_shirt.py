6dfdc6de185356017e29ab675322bf3739f1f599
Wenlong Xiong <wenlongx@gmail.com>
2015-07-27 15:21:17 -0700

    Initial Commit

---
+++ drop_shirt.py
@@ -0,0 +1,80 @@
+# Takes arbitrary command line arguments
+#   1) string; path to OBJ shirt model
+#   2 and up) int; vertices to pin
+
+# Generates an OBJ file in the current directory
+#   1) an OBJ file called out_vtx.obj, where vtx is the vertex number
+
+import sys
+sys.path.insert(0, "/Applications/Autodesk/maya2015/Maya.app/Contents/Frameworks/Python.framework/Versions/Current/lib/python2.7/site-packages")
+import maya.standalone
+maya.standalone.initialize()
+import maya.cmds as cmds
+import maya.mel as mel
+import os
+import time
+
+OUT_NAME = "out"
+STARTFRAME = 0
+ENDFRAME = 300
+VERTICES = []
+for x in range(len(sys.argv)-2):
+    VERTICES.append(int(sys.argv[x+2]))
+
+# Import shirt, convert to nCloth
+def to_nCloth():
+    cmds.loadPlugin("objExport")
+    mel.eval('file -f -options "mo=1" -ignoreVersion -typ "OBJ" -o "%s";' % sys.argv[1])
+    cmds.select(clear = True)
+    cmds.select("shirt")
+    mel.eval("doCreateNCloth 0;")
+
+# Pin vertices
+def pin_vertex(vtx):
+    """Takes in a vertex number"""
+    shirt_vertex="shirt.vtx[" + str(vtx) + "]"
+    cmds.select(clear = True)
+    cmds.select(shirt_vertex)
+    mel.eval("createNConstraint transform 1;")
+
+# Drop Shirt model
+def drop_simulation():
+    start = time.time()
+    cmds.playbackOptions(min = STARTFRAME, max = ENDFRAME)
+    cmds.currentTime(STARTFRAME)
+    cmds.select(clear = True)
+    cmds.select("shirt")
+    cmds.bakeResults(simulation = True, controlPoints = True, shape = True, time = (STARTFRAME, ENDFRAME))
+    cmds.currentTime(ENDFRAME)
+    end = time.time()
+    return end-start
+
+# Convert to OBJ
+def export_obj(name):
+    obj_path = os.getcwd() + "/out/" + name
+    for x in VERTICES:
+        obj_path += "_" + str(x)
+    obj_path += ".obj"
+    print("Saving file as " + obj_path)
+    cmds.select(clear = True)
+    cmds.select("shirt")
+    mel.eval('file -force -options "groups=1;ptgroups=1;materials=1;smoothing=1;normals=1" -typ "OBJexport" -pr -es "%s";' % obj_path)
+
+
+# Main program
+print("Running simulation on file " + sys.argv[1])
+print("Simulation performed on vertices numbered " + str(VERTICES))
+to_nCloth()
+for x in VERTICES:
+    pin_vertex(x)
+time_elapsed = drop_simulation()
+print("Simulation took " + str(time_elapsed) + " seconds to run")
+export_obj(OUT_NAME)
+
+
+'''
+# Save a Maya Binary for Reference
+cmds.file(rename = "out_2.mb")
+cmds.file(save = True, type = "mayaBinary")
+os.system("cp /private/var/root/Documents/maya/projects/default/out_2.mb ~/Desktop/out_2.mb")
+'''
---
+++ generate_shirt.py
@@ -0,0 +1,63 @@
+# Don't understand this
+# http://knowledge.autodesk.com/support/maya/learn-explore/caas/CloudHelp/cloudhelp/2015/ENU/Maya/files/Python-Python-from-an-external-interpreter-htm.html
+
+# Takes 1 command line arguments
+#   1) string; path to CSV point file
+
+# Generates in your current directory
+#   1) an OBJ file called out.obj
+#   2) a Maya Binary file called out.mb
+
+import sys
+sys.path.insert(0, "/Applications/Autodesk/maya2015/Maya.app/Contents/Frameworks/Python.framework/Versions/Current/lib/python2.7/site-packages")
+import maya.standalone
+maya.standalone.initialize()
+import maya.cmds as cmds
+import maya.mel as mel
+import csv
+import os
+
+SUBDIVISIONS = 3
+OUT_NAME = "shirt"
+
+# Read from CSV file, store in points
+def read_csv():
+    """Opens CSV file and stores each points (tuple) in a list"""
+    points = []
+    with open(sys.argv[1], "rU") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if len(row) > 3:
+                print("Points in CSV file are greater than 3 dimensions")
+                sys.exit(0)
+            # If set of points is 2 dimensional, autogenerate the 3rd dimension
+            elif len(row) == 2:
+                row.append(['0'])
+            points.append(tuple(map(float, row)))
+    return points
+
+# Create polygon called shirt based on points
+def generate(pts):
+    """Takes in a list of tuples (set of 3D points) and generates a polygon model"""
+    cmds.polyCreateFacet(name="shirt", p=points)
+    cmds.polyTriangulate()
+    cmds.polySubdivideFacet(dv=SUBDIVISIONS)
+    cmds.polyTriangulate()
+
+# Exports file as a Maya Binary
+def export_mb(name):
+    cmds.file(rename = name + ".mb")
+    cmds.file(save = True, type = "mayaBinary")
+    os.system("cp /private/var/root/Documents/maya/projects/default/scenes/" + name + ".mb " + os.getcwd() + "/" + name + ".mb")
+
+# Exports file as an OBJ
+def export_obj(name):
+    cmds.loadPlugin('objExport')
+    obj_path = os.getcwd() + "/" + name + ".obj"
+    mel.eval('file -force -options "groups=1;ptgroups=1;materials=1;smoothing=1;normals=1" -type "OBJexport" -pr -ea "%s";' % obj_path)
+
+
+# Main Program
+points = read_csv()
+generate(points)
+export_obj(OUT_NAME)
